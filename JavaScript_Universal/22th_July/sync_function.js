2d8aa37cc1dc243dd8d3aa2098bccc19978a2e2b
Nirnjan Patel <DevNirnjanPatel.com>
2022-07-25 12:34:59 +0530

    Everything Added till 23 july

---
+++ JavaScript_Universal/22th_July/sync_function.js
@@ -0,0 +1,12 @@
+//Synchronous function
+
+console.log("Before function execution");
+
+function demo()
+{
+ console.log("Demo function invoked");
+}
+
+demo();
+
+console.log("After function execution");
---
+++ JavaScript_Universal/23th_July/callback1.js
@@ -0,0 +1,17 @@
+//callback : Asynchronous
+console.log("Before function execution");
+
+function demo(cb)
+{ 
+ console.log("Demo function executed");
+ setTimeout(cb,3000);
+}
+
+demo(()=>{
+ console.log("Callback function execution");
+});
+
+console.log("After function execution");
+
+
+
