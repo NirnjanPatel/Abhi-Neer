f604e9c539aeb5d8c75473898877cc235879ab0b
Nirnjan Patel <DevNirnjanPatel.com>
2022-07-29 12:36:19 +0530

    .

--- ES6/27th-July/encapsulation1.js
+++ ES6/27th-July/encapsulation1.js
@@ -1,25 +1,22 @@
 //encapsulation : bug
-class Employee
-{
- setEmployee(eno,enm)
- {
-    this.eno=eno;
-    this.enm=enm;
- }
- showEmployee()
- {
-    console.log("Eno :",this.eno);
-    console.log("Enm :",this.enm);
- }
-} 
+class Employee {
+   setEmployee(eno, enm) {
+      this.eno = eno;
+      this.enm = enm;
+   }
+   showEmployee() {
+      console.log("Eno :", this.eno);
+      console.log("Enm :", this.enm);
+   }
+}
 
 var obj = new Employee();
-obj.setEmployee(1001,"Jarvis");
+obj.setEmployee(1001, "Jarvis");
 
 console.log("Before changes :-");
 obj.showEmployee();
 
-obj.eno=0;
+obj.eno = 0;
 
 console.log("After changes :-");
 obj.showEmployee();
