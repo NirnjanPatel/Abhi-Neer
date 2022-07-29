5bed294fb0c6a39b309e38158388a2ad5aac0b15
Nirnjan Patel <DevNirnjanPatel.com>
2022-07-29 11:56:32 +0530

    ALL PROGRAMMING CONCEPTS and their PRACTICLES and PROJECTS of FRONTEND, BACKEND and DATABASE of WEB DEVELOPMENT, WEB APP DEVELOPMENT (including MERN Full Stack Development).

---
+++ ES6/27th-July/encapsulation.js
@@ -0,0 +1,20 @@
+//encapsulation
+class Employee
+{
+ setEmployee(eno,enm)
+ {
+    this.eno=eno;
+    this.enm=enm;
+ }
+ showEmployee()
+ {
+    console.log("Eno :",this.eno);
+    console.log("Enm :",this.enm);
+ }
+} 
+
+var obj = new Employee();
+obj.setEmployee(1001,"Jarvis");
+obj.showEmployee();
+
+console.log("Eno outside class :"+obj.eno);
